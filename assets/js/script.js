2e271a6893a6224b528d57d857807caad8dce23b
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-07-22 14:00:32 -0400

    set js variables and html

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var headerEl = document.querySelector("#header");
+var viewscoreEl = document.querySelector("#view-highscore");
+var timerEl = document.querySelector("#timer");
+
+var containerEl = document.querySelector("#quiz-container");
+var maincontentEl = document.querySelector("#main");
+
+var welcomePageEl = document.querySelector("#onboarding");
+var highScoreEl = document.querySelector("#highscore");
+
+var scoreResultsEl = document.querySelector("#results");
+var answersEl = document.querySelector("#answers");
